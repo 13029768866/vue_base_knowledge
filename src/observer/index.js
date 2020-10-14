a60ba73ad8f84eddc287f737740e116e4ef04152
woodensRangerWZJ <13029768866@163.com>
2020-10-14 23:56:20 +0800

    observer初始化

--- src/observer/index.js
+++ src/observer/index.js
@@ -1,5 +1,14 @@
+class Observer {
+  constructor(value){
+    this.walk(value)
+  }
+  walk(){
 
+  }
+}
 
 export function observer(data) {
-
+  /* 类型判断 */
+  if(typeof data === 'object' || data === null) return;
+  return new Observer(data);
 }
