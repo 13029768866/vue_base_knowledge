2c0f5635e7754f099ce25c9cd2506771acae287c
woodensRangerWZJ <13029768866@163.com>
2020-10-27 00:23:44 +0800

    -m复习

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import {initMixin} from "./init";
 
 
-function Vue(options){
-  /* 1、options初始化 */
-  this._init(options);
+function Vue(options) {
+//  2、执行options初始化
+  this._init(options)
 }
 
-/* 初始化(原型上挂载方法) */
+// 1、原型上挂载初始化方法
 initMixin(Vue);
-
-
 export default Vue;
